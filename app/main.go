bf01e43f0730992c5c803f17d816fc3f95f41b1d
Muaaz Deyab <mo3athmd20@gmail.com>
2023-12-23 21:11:47 +0200

    Add Header to the Message

--- app/main.go
+++ app/main.go
@@ -1,13 +1,103 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+
 	// Uncomment this block to pass the first stage
 	"net"
 )
 
+type Message struct {
+	DnsHeader Header
+}
+
+type Header struct {
+	ID      uint16
+	Flags   DnsMsgFlags
+	QDCount uint16
+	ANCount uint16
+	NSCount uint16
+	ARCount uint16
+}
+
+type DnsMsgFlags struct {
+	QR     bool
+	OPCode uint8
+	AA     bool
+	TC     bool
+	RD     bool
+	RA     bool
+	Z      uint8
+	Rcode  uint8
+}
+
+func CreateResponse() Message {
+	return Message{
+		DnsHeader: Header{
+			ID: 1234,
+			Flags: DnsMsgFlags{
+				QR:     true,
+				OPCode: 0x0,
+				AA:     false,
+				TC:     false,
+				RD:     false,
+				RA:     false,
+				Z:      0x0,
+				Rcode:  0x0,
+			},
+			QDCount: 0x0,
+			ANCount: 0x0,
+			NSCount: 0x0,
+			ARCount: 0x0,
+		},
+	}
+}
+
+// 0 :Id  0000: Opcode  0: AA  0: TC  0: RD  0:RA  000:Z   0000 : Rcode
+
+func (header Message) serialize() []byte {
+	return header.DnsHeader.serialize()
+}
+
+func (msg Header) serialize() []byte {
+	result := make([]byte, 12)
+
+	binary.BigEndian.PutUint16(result[:2], msg.ID)
+	var flags uint16
+
+	if msg.Flags.QR {
+		flags |= 1 << 15
+	}
+	flags |= uint16(msg.Flags.OPCode) << 11
+
+	if msg.Flags.AA {
+		flags |= 1 << 10
+	}
+	if msg.Flags.TC {
+		flags |= 1 << 9
+	}
+	if msg.Flags.RD {
+		flags |= 1 << 8
+	}
+	if msg.Flags.RA {
+		flags |= 1 << 7
+	}
+
+	flags |= uint16(msg.Flags.Z) << 4
+	flags |= uint16(msg.Flags.Rcode)
+	binary.BigEndian.PutUint16(result[2:4], flags)
+	binary.BigEndian.PutUint16(result[4:6], msg.QDCount)
+	binary.BigEndian.PutUint16(result[6:8], msg.ANCount)
+	binary.BigEndian.PutUint16(result[8:10], msg.NSCount)
+	binary.BigEndian.PutUint16(result[10:12], msg.ARCount)
+	return result
+
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
+
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
@@ -38,7 +128,8 @@ func main() {
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
 		// Create an empty response
-		response := []byte{}
+		response := CreateResponse().serialize()
+
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
